568b318aadf35ea432e056a4dccb92750ffcd8b9
GriShafir <67635528+GriShafir@users.noreply.github.com>
2021-08-01 16:47:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function right_arrow() {
         to = 0;
         obj.src = mas[to];
     }
+    setCookie("foo", mas[to], "", "/");
 }
 
 function left_arrow() {
@@ -19,9 +20,36 @@ function left_arrow() {
         to = mas.length - 1;
         obj.src = mas[to];
     }
+    setCookie("foo", mas[to], "", "/");
 }
 
-/*function setCookie (name, value, expires, path, domain, secure) {
+function setCookie (name, value, expires, path, domain, secure) {
     if (!name || !value) return false;
-    var 
-}*/
+        var str = name + "=" +encodeURIComponent(value);
+            if (expires) str += "; expires=" + expires.toGMTString();
+            if (path) str += "; path=" + path;
+            if (domain) str += "; domain=" + domain;
+            if (secure) str += "; secure";
+    document.cookie = str;
+    return true;
+}
+
+function getCookie (name) {
+    var pattern = "(?:; )?" + name + "=([^;]*);?";
+    var regexp = new RegExp(pattern);
+    if (regexp.test(document.cookie)) {
+        return decodeURIComponent(RegExp["$1"]);
+        return false;
+    }
+}
+
+function Load () {
+    var cook_val = getCookie("foo");
+    for (var i = 0; i < mas.length; i++) {
+        if (mas[i] == cook_val) {
+            document.querySelector("#img").src = mas[i];
+            to = i;
+            break;
+        }
+    }
+}
